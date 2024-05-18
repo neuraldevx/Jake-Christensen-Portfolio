9b7fe0cde3bc65d9b2b806e40d1cffe5a7d2e035
jake <83853752+neuraldevx@users.noreply.github.com>
2024-05-18 10:57:02 +0000

    fixed deployment

--- app/api/blog-posts/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import { getBlogPosts } from 'app/blog/utils';
-
-export async function GET() {
-  const posts = getBlogPosts();
-  return NextResponse.json(posts);
-}
